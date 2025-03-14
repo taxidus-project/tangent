d807654b0d4a6ec7fa62d2e312ee3086343ec821
Hkaar <HkaarDev@gmail.com>
2025-03-14 21:01:41 +0800

    Added config

---
+++ src/config.lua
@@ -0,0 +1,18 @@
+return {
+    APP_NAME = "Tangent Playground",
+    APP_VENDOR = "Trimedge",
+    APP_VERSION = "1.0",
+
+    ENGINE_VERSION = "1.0.0",
+    ENGINE_VENDOR = "Trimedge",
+    ENGINE_PLATFORM = "lua:local",
+
+    ENGINE_ENV = "local",
+    ENGINE_DEBUG = false,
+
+    ENGINE_RESOURCE_URL = "local",
+    ENGINE_STORE_MAX_KEYS = 128,
+    ENGINE_MAX_PLAYERS = 8,
+
+    ENGINE_EXTENSIONS = {}
+}
