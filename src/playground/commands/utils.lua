1150ae7750b24568f7bab6847d0186b4f24ef048
Hkaar <HkaarDev@gmail.com>
2025-03-15 23:02:49 +0800

    Updated command utils

--- src/playground/commands/utils.lua
+++ src/playground/commands/utils.lua
@@ -20,9 +20,10 @@ end
 --- Extracts options from the provided arguments denoted
 --- with the '-' character at the start of the argument
 ---
---- @param args metatable
---- @return metatable
+--- @param args table
+--- @return table
 local function get_options(args)
+    --- TODO: Maybe seperate flags and values?
     local options = {}
 
     for _, v in pairs(args) do
@@ -32,7 +33,7 @@ local function get_options(args)
         local exists_eq = string.find(v, '=')
 
         if exists_eq then
-            c_val = string.sub(v, exists_eq, string.len(v))
+            c_val = string.sub(v, exists_eq + 1, string.len(v))
         end
 
         options[c_key] = c_val
@@ -41,7 +42,28 @@ local function get_options(args)
     return options
 end
 
+--- Describes on how to create a help option for a command
+---
+--- @class CmdHelpOption
+--- @field title string
+--- @field desc string
+
+--- Generate a help menu quickly for a command
+---
+--- @param desc string
+--- @param struct string
+--- @param options CmdHelpOption[]
+local function help(desc, struct, options)
+    print(string.format("%s\n", desc))
+    print(string.format("%s\n", struct))
+
+    for _, v in ipairs(options) do
+        print(string.format("%-20s %s", v.title, v.desc))
+    end
+end
+
 return {
     cmd = cmd,
-    get_options = get_options
+    get_options = get_options,
+    help = help,
 }
